375084592d111e0000430e3ab268ee30736e4a68
ankita109 <dubeyankita107@gmail.com>
2023-01-28 16:15:52 +0530

    React project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import Heading from './Heading.js'
+import Section1 from './Section1.js'
+import Section2 from './Section2.js'
+
+
+ function App() {
+  return (
+    <div className="App">
+      <Heading />
+      <Section1 />
+      <Section2 />
+    </div>
+  );
+}
+
+export default App;  
+   
+
+
+
+
