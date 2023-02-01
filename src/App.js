c154ef9b5413da378352f34deb859a194d688a74
ankita109 <dubeyankita107@gmail.com>
2023-02-01 16:17:02 +0530

    form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 
-import React from 'react'
+import React,{useState} from 'react'
 
 
 export default function   App() {
+  const [Name, setName] = useState(" ");
+  const [Email, setEmail] = useState(" ");
+  const [Password, setPassword] = useState(" ");
+
+  function handleName(e){
+    console.log(Name,"  ");
+     setName(e.target.value);
+  }
  
+  function handleEmail(f){
+    console.log(Email,"  ");
+     setName(f.target.value);
+  }
+
+  function handlePassword(g){
+    console.log(Password,"  ");
+     setName(g.target.value);
+  }
   return (
     <div className="App" >
       <h1>FORM</h1>
      <form>
-      <label>Name : <input type="text" /></label><br/><br/>
-      <label>Email : <input type="text" /></label><br/><br/>
-      <label>Password : <input type="text" /></label><br/><br/>
+      <label>Name : <input type="text" onChange={handleName} /></label><br/><br/>
+      <label>Email : <input type="email" onChange={handleEmail}/></label><br/><br/>
+      <label>Password : <input type="number" onChange={handlePassword}/></label><br/><br/>
       <button>Submit</button>
      </form>
     </div>
